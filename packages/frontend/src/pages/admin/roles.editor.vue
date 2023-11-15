b118320eb5d0edb59c517fb65647c9e54c722200
ありす <alice.164@outlook.jp>
2023-11-15 18:06:11 +0900

    Update roles.editor.vue

--- packages/frontend/src/pages/admin/roles.editor.vue
+++ packages/frontend/src/pages/admin/roles.editor.vue
@@ -195,7 +195,11 @@ SPDX-License-Identifier: AGPL-3.0-only
 						<template #label>{{ i18n.ts.enable }}</template>
 					</MkSwitch>
 					<MkRange v-model="role.policies.canEditNote.priority" :min="0" :max="2" :step="1" easing :textConverter="(v) => v === 0 ? i18n.ts._role._priority.low : v === 1 ? i18n.ts._role._priority.middle : v === 2 ? i18n.ts._role._priority.high : ''">
-			<MkFolder v-if="matchQuery([i18n.ts._role._options.canImportNotes, 'canImportNotes'])">
+				</div>
+			</MkFolder>
+
+			<MkFolder>
+				<MkFolder v-if="matchQuery([i18n.ts._role._options.canImportNotes, 'canImportNotes'])">
 				<template #label>{{ i18n.ts._role._options.canImportNotes }}</template>
 				<template #suffix>
 					<span v-if="role.policies.canImportNotes.useDefault" :class="$style.useDefaultLabel">{{ i18n.ts._role.useBaseValue }}</span>
